0d5940f4790ed543f2e9205848edcdafefea30ce
IT24102795 <it24102795@my.sliit.lk>
2025-05-14 14:00:33 +0530

    Create Event class

--- src/main/java/com/example/event_booking_project_90/model/Event.java
+++ src/main/java/com/example/event_booking_project_90/model/Event.java
@@ -1,4 +1,61 @@
 package com.example.event_booking_project_90.model;
 
 public class Event {
+    private String id;
+    private String name;
+    private String date;
+    private String location;
+
+
+    public Event(String id, String name, String date, String location) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.location = location;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void displayDetails(){
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+
+    }
+
+
+    @Override
+    public String toString() {
+        return id + "," + name + "," + date + "," + location + "\n";
+    }
+
 }
