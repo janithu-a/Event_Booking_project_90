7d08a784754bfb52055cc4f1eb1f22b7a8b6c383
IT24102795 <it24102795@my.sliit.lk>
2025-05-20 22:41:48 +0530

    Update Photography.java

--- src/main/java/com/example/event_booking_project_90/model/Photography.java
+++ src/main/java/com/example/event_booking_project_90/model/Photography.java
@@ -19,4 +19,52 @@ public class Photography {
         this.description = description;
         this.username = username;
     }
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
